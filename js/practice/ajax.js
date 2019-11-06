3a715c59f8924d39368af1789df10c3522045b5a
moorekwon <raccoonhj33@gmail.com>
2019-11-06 22:57:27 +0900

    191106 todo list v0.2 makeup class homework - toggle checked

---
+++ js/practice/ajax.js
@@ -0,0 +1,63 @@
+// const o = { name: 'Lee', gender: 'male', age: 20 };
+// console.log(typeof o);
+
+// const strObject = JSON.stringify(o);
+// console.log(typeof strObject);
+// console.log(strObject);
+
+// const arr = [1, 5, 'false'];
+// console.log(typeof arr);
+
+// const strArray = JSON.stringify(arr);
+// console.log(typeof strArray);
+// console.log(strArray);
+
+// const obj = JSON.parse(strObject);
+// console.log(typeof obj);
+// console.log(obj);
+
+// const objArray = JSON.parse(strArray);
+// console.log(typeof objArray);
+// console.log(objArray);
+
+// const o = { name: 'Lee', gender: 'male', age: 20 };
+
+// const strObject = JSON.stringify(o);
+// console.log(typeof strObject, strObject);
+
+// const strPrettyObject = JSON.stringify(o, null, 2);
+// console.log(typeof strPrettyObject, strPrettyObject);
+
+// function filter(key, value) {
+//     return typeof value === 'number' ? undefined : value;
+// }
+
+// const strFilteredObject = JSON.stringify(o, filter, 2);
+// console.log(typeof strFilteredObject, strFilteredObject);
+
+// const arr = [1, 5, 'false'];
+
+// const strArray = JSON.stringify(arr);
+// console.log(typeof strArray, strArray);
+
+// function replaceToUpper(key, value) {
+//     console.log(typeof value);
+//     return value.toString().toUpperCase();
+// }
+
+// const strFilteredArray = JSON.stringify(arr, replaceToUpper);
+// console.log(typeof strFilteredArray, strFilteredArray);
+
+// const todos = [
+//     { id: 1, content: 'HTML', completed: true },
+//     { id: 2, content: 'CSS', completed: true },
+//     { id: 3, content: 'JavaScript', completed: false }
+// ];
+
+// const str = JSON.stringify(todos);
+// console.log(typeof str, str);
+
+// const parsed = JSON.parse(str);
+// console.log(typeof parsed, parsed);
+
+
